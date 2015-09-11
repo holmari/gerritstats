05653e649c5dd6cbaa696dc221830769ad02deb5
Lasse Holmstedt <lasse.holmstedt@gmail.com>
2015-09-11 23:30:49 +0200

    Replace custom command line parsing with JCommander
    Done to reduce the parsing code and make it straightforward to make
    modifications. There's a bit too many arguments now, and one way to
    solve it would be to split the tool into two: a data downloader and
    a stats parser. To unify the code of two tools, a command line parser
    is used as a pre-step.

--- src/main/java/com/holmsted/gerrit/CommandLineParser.java
+++ src/main/java/com/holmsted/gerrit/CommandLineParser.java
@@ -1,95 +1,137 @@
 package com.holmsted.gerrit;
 
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
+@SuppressWarnings("unused")
 public class CommandLineParser {
 
     private static final String DEFAULT_OUTPUT_DIR = "out";
 
+    public static class ServerAndPort {
+        private String serverName;
+        private int serverPort;
+
+        @Override
+        public String toString() {
+            return "None. Required if no filename is specified.";
+        }
+
+        public static class Converter implements IStringConverter<ServerAndPort> {
+            @Override
+            public ServerAndPort convert(String value) {
+                ServerAndPort result = new ServerAndPort();
+                result.serverName = value;
+                int portSeparator = result.serverName.indexOf(':');
+                if (portSeparator != -1) {
+                    result.serverPort = Integer.valueOf(result.serverName.substring(portSeparator + 1));
+                    result.serverName = result.serverName.substring(0, portSeparator);
+                }
+                return result;
+            }
+        }
+    }
+
+    public static class OutputTypeConverter implements IStringConverter<OutputType> {
+        @Override
+        public OutputType convert(String value) {
+            return OutputType.fromTypeString(value);
+        }
+    }
+
+    public static class OutputConverter implements IStringConverter<Output> {
+        @Override
+        public Output convert(String value) {
+            return Output.fromString(value);
+        }
+    }
+
+    @Parameter(names = "--file",
+            description = "read output from file. The file is expected to be a json file as output by this tool.")
     private String filename;
-    private String serverName;
-    private int serverPort;
+
+    @Parameter(names = "--server",
+            description = "read output from Gerrit server url and given port. If port is omitted, defaults to 29418.",
+            arity = 1,
+            converter = ServerAndPort.Converter.class)
+    @Nonnull
+    private final ServerAndPort serverAndPort = new ServerAndPort();
+
+    @Parameter(names = "--project", description = "specifies the Gerrit project from which to retrieve stats. "
+            + "If omitted, stats will be retrieved from all projects.")
     private String projectName;
+
+    @Parameter(names = "--query-output-file", description = "if specified, the output of the query"
+            + "will be written into  the specified file, to be used later with e.g. --file switch.")
     private String outputFile;
+
+    @Parameter(names = "--limit", description = "The number of commits which to retrieve from the server. "
+            + "If omitted, stats will be retrieved until no further records are available.")
     private int limit = GerritStatReader.NO_COMMIT_LIMIT;
+
+    @Parameter(names = "--exclude",
+            description = "If specified, the comma-separated list of identities "
+                    + " will be excluded from all generated statistics.")
     @Nonnull
     private final List<String> excludedEmails = new ArrayList<>();
+    @Parameter(names = "--branches",
+            description = "If specified, only the comma-separated list of branches "
+            + "will be included for analysis. If omitted, all available branches will be inspected.")
     @Nonnull
     private final List<String> includedBranches = new ArrayList<>();
+
+    @Parameter(names = "--include",
+            description = "If specified, only the comma-separated list of identities "
+            + "will be included in generated statistics. If both --include and --exclude are "
+            + "specified, --exclude is ignored.")
     @Nonnull
     private final List<String> includedEmails = new ArrayList<>();
+
+    @Parameter(names = "--output-type",
+            description = "If specified, the output will be provided in the specified format. Defaults to HTML.",
+            arity = 1,
+            converter = OutputTypeConverter.class)
     @Nonnull
     private OutputType outputType = OutputType.HTML;
 
+    @Parameter(names = "--commit-patch-set-count-threshold",
+            description = "If specified, all commit URLs exceeding the given patch set count will be listed "
+            + "in the per-person data. If -1 is set, no listing is provided.")
     private int commitPatchSetCountThreshold = 5;
+
+    @Parameter(names = "--output",
+            description = "If specified, the output will be either a list of all review comments in CSV format, "
+            + "or a per-person data set.",
+            arity = 1,
+            converter = OutputConverter.class)
     private Output output = Output.PER_PERSON_DATA;
+
+    @Parameter(names = "--output-dir",
+            description = "If specified, the output will be generated into the given directory.")
     @Nonnull
     private String outputDir = DEFAULT_OUTPUT_DIR;
 
+    @Nonnull
+    private final JCommander jCommander = new JCommander(this);
+
     public boolean parse(String[] args) {
-        boolean hasSyntaxError = false;
-        for (int i = 0; i < args.length; ++i) {
-            String arg = args[i];
-            boolean isNotAtEnd = (i < args.length - 1);
-            if (arg.equals("--file") && isNotAtEnd) {
-                filename = args[i + 1];
-                ++i;
-            } else if (arg.equals("--server") && isNotAtEnd) {
-                serverName = args[i + 1];
-                int portSeparator = serverName.indexOf(':');
-                if (portSeparator != -1) {
-                    serverPort = Integer.valueOf(serverName.substring(portSeparator + 1));
-                    serverName = serverName.substring(0, portSeparator);
-                }
-                ++i;
-            } else if (arg.equals("--project") && isNotAtEnd) {
-                projectName = args[i + 1];
-                ++i;
-            } else if (arg.equals("--query-output-file") && isNotAtEnd) {
-                outputFile = args[i + 1];
-                ++i;
-            } else if (arg.equals("--limit") && isNotAtEnd) {
-                try {
-                    limit = Integer.parseInt(args[i + 1]);
-                    ++i;
-                } catch (NumberFormatException nfe) {
-                    hasSyntaxError = true;
-                }
-            } else if (arg.equals("--exclude") && isNotAtEnd) {
-                String[] emails = args[i + 1].split(",");
-                Collections.addAll(excludedEmails, emails);
-                ++i;
-            } else if (arg.equals("--include") && isNotAtEnd) {
-                String[] emails = args[i + 1].split(",");
-                Collections.addAll(includedEmails, emails);
-                ++i;
-            } else if (arg.equals("--branches") && isNotAtEnd) {
-                String[] branches = args[i + 1].split(",");
-                Collections.addAll(includedBranches, branches);
-                ++i;
-            } else if (arg.equals("--output-type") && isNotAtEnd) {
-                outputType = OutputType.fromTypeString(args[i + 1]);
-                ++i;
-            } else if (arg.equals("--output") && isNotAtEnd) {
-                output = checkNotNull(Output.fromString(args[i + 1]));
-                ++i;
-            } else if (arg.equals("--output-dir") && isNotAtEnd) {
-                outputDir = resolveOutputDir(checkNotNull(args[i + 1]));
-            } else if (arg.equals("--commit-patch-set-count-threshold") && isNotAtEnd) {
-                commitPatchSetCountThreshold = Integer.parseInt(args[i + 1]);
-                ++i;
-            }
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException e) {
+            e.printStackTrace();
+            return false;
         }
 
-        return (filename != null || serverName != null) && !hasSyntaxError;
+        return (filename != null || serverAndPort.serverName != null);
     }
 
     @Nonnull
@@ -112,7 +154,7 @@ public class CommandLineParser {
 
     @Nullable
     public String getServerName() {
-        return serverName;
+        return serverAndPort.serverName;
     }
 
     @Nullable
@@ -121,7 +163,7 @@ public class CommandLineParser {
     }
 
     public int getServerPort() {
-        return serverPort;
+        return serverAndPort.serverPort;
     }
 
     public int getCommitLimit() {
@@ -160,5 +202,9 @@ public class CommandLineParser {
 
     public int getCommitPatchSetCountThreshold() {
         return commitPatchSetCountThreshold;
+    }
+
+    public void printUsage() {
+        jCommander.usage();
     }
 }
--- src/main/java/com/holmsted/gerrit/GerritStatsMain.java
+++ src/main/java/com/holmsted/gerrit/GerritStatsMain.java
@@ -13,43 +13,8 @@ public class GerritStatsMain {
     public static void main(String[] args) {
         CommandLineParser commandLine = new CommandLineParser();
         if (!commandLine.parse(args)) {
-            System.err.println("Reads and outputs Gerrit statistics.");
-            System.err.println("Usage: GerritStats.jar [--file file]|"
-                    + "[--server server:port [--project project] [--limit n] [--query-output-file file]]");
-            System.err.println("      [--branches master,feature1]");
-            System.err.println("      [--include joe@root.com,jeff@foo.bar..,xyzzy@inter.net]");
-            System.err.println("      [--exclude joe@root.com,jeff@foo.bar..,xyzzy@inter.net]");
-            System.err.println("      [--commit-patch-set-count-threshold n]");
-            System.err.println("      [--output-type plain|csv|html]");
-            System.err.println("      [--output review-comment-csv|per-person-data]");
-            System.err.println("      [--output-dir outputDir]");
-            System.err.println();
-            System.err.println(" --file file: read output from file");
-            System.err.println(String.format(" --server url:port: read output from Gerrit server url and given port."
-                    + " If port is omitted, defaults to %s.",
-                    GerritStatReader.GERRIT_DEFAULT_PORT));
-            System.err.println(" --project project: specifies the Gerrit project from which to retrieve stats. "
-                    + "If omitted, stats will be retrieved from all projects.");
-            System.err.println(" --limit n: The number of commits which to retrieve from the server. "
-                    + "If omitted, stats will be retrieved until no further records are available.");
-            System.err.println(" --query-output-file file: if specified, the output of the query will be written into "
-                    + "the specified file, to be used later with e.g. --file switch.");
-            System.err.println(" --branches branches: if specified, only the comma-separated list of branches "
-                    + "will be included for analysis. If omitted, all available branches will be inspected.");
-            System.err.println(" --include list-of-people: if specified, only the comma-separated list of identities "
-                    + " will be included in generated statistics. If both --include and --exclude are "
-                    + "specified, --exclude is ignored.");
-            System.err.println(" --exclude list-of-people: if specified, the comma-separated list of identities "
-                    + " will be excluded from all generated statistics.");
-            System.err.println("--commit-patch-set-count-threshold: If specified, all commit URLs "
-                    + "exceeding the given patch set count will be listed in the per-person data. Defaults to 5."
-                    + "If -1 is set, no listing is provided.");
-            System.err.println(" --output-type: if specified, the output will be provided in the specified format."
-                    + "Defaults to HTML.");
-            System.err.println(" --output: If specified, the output will be either a list of all review comments in "
-                    + "CSV format, or a per-person data set. Defaults to per-person-data.");
-            System.err.println("--output-dir: If specified, the output will be generated into the given directory. "
-                    + "Defaults to out/.");
+            System.out.println("Reads and outputs Gerrit statistics.");
+            commandLine.printUsage();
             System.exit(1);
             return;
         }
