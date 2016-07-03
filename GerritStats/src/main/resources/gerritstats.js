1b3036081a06aed01d760804121332f3aa61a3cd
Lasse Holmstedt <lasse.holmstedt@gmail.com>
2016-07-02 17:05:26 -0700

    Duration formatting: Don't write days / hours / minutes if zero

--- GerritStats/src/main/resources/gerritstats.js
+++ GerritStats/src/main/resources/gerritstats.js
@@ -76,7 +76,18 @@ function formatPrintableDuration(durationMsec) {
     var hours = parseInt(durationInSecs / (60 * 60) % 24);
     var minutes = parseInt(durationInSecs / (60) % 60);
 
-    return days + "d " + hours + "h " + minutes + "min";
+    var daysPart = days > 0 ? days + 'd ' : '';
+    var hoursPart = hours > 0 ? hours + 'h ' : '';
+    var minPart = minutes > 0 ? minutes + 'min' : '';
+    if (!days && !hours && minutes == 0 && durationInSecs > 0) {
+        minPart = '< 1min';
+    }
+
+    if (durationInSecs > 0) {
+        return daysPart + hoursPart + minPart;
+    } else {
+        return '&dash;';
+    }
 }
 
 function getShortPrintableName(identity) {
