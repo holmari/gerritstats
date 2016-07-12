d33c183cf58ca406ec606be4d176f7e3673f42ec
Lasse Holmstedt <lasse.holmstedt@gmail.com>
2016-07-11 21:18:02 -0700

    Index page: don't highlight cells that are not necessary to highlight
    This includes comments per commit for a user that has zero commits,
    or users with zero self-reviews, or when average time in code review is
    zero.

--- GerritStats/src/main/resources/tablecellhighlighter.js
+++ GerritStats/src/main/resources/tablecellhighlighter.js
@@ -8,26 +8,40 @@ function TableCellHighlighter(overviewUserdata, dataKey) {
     this.colorsBottomEntries = ['#ff90d5', '#ffaadf', '#ffacce', '#ffbdcd', '#ffcecc']
 
     this.useAscendingSort = false;
-
     this.ignoreZeroes = false;
+    this.highlightNegativeEntries = true;
+    this.highlightPositiveEntries = true;
 
     this.sortedData = null;
+    this.ignoreFunction = null;
+
+    this.isFiltered = function(element, key) {
+        if (this.ignoreZeroes && !element[key]) {
+            return true;
+        }
+        if (this.ignoreFunction && this.ignoreFunction(element, key)) {
+            return true;
+        }
+        return false;
+    }
 
     /** Returns a sorted, unique list of values from a given array, for the given key. */
-    function sortUniqueValues(arrayToFilter, key) {
+    this.sortUniqueValues = function() {
         var descendingSorter = function(l, r) {
-            return r[key] - l[key];
+            return r[dataKey] - l[dataKey];
         };
         var ascendingSorter = function(l, r) {
-            return l[key] - r[key];
+            return l[dataKey] - r[dataKey];
         };
-        var sortedArray = arrayToFilter.concat().sort(
+        var sortedArray = overviewUserdata.concat().sort(
             this.useAscendingSort ? ascendingSorter : descendingSorter);
 
+        var that = this;
         var filteredArray = [];
         sortedArray.forEach(function(element) {
-            if (filteredArray.length == 0 || element[key] != filteredArray[filteredArray.length - 1]) {
-                filteredArray.push(element[key]);
+            if ((filteredArray.length == 0 || element[dataKey] != filteredArray[filteredArray.length - 1])
+                && !that.isFiltered(element, dataKey)) {
+                filteredArray.push(element[dataKey]);
             }
         });
         return filteredArray;
@@ -41,43 +55,55 @@ function TableCellHighlighter(overviewUserdata, dataKey) {
 
     this.highlightDescending = function(value) {
         var topRangeCutoffIndex = Math.min(this.colorsTopEntries.length - 1, this.sortedData.length - 1);
-        if (value >= this.sortedData[topRangeCutoffIndex]) {
+        if (this.highlightPositiveEntries && value >= this.sortedData[topRangeCutoffIndex]) {
             var colorIndex = this.sortedData.findIndex(indexFinder(value));
             return 'style=background:' + this.colorsTopEntries[colorIndex];
-        } else if (this.sortedData.length >= this.colorsBottomEntries.length
-            && value <= this.sortedData[this.sortedData.length - this.colorsBottomEntries.length])
-        {
+        }
+
+        var leastBottomItemIndex = this.sortedData.length >= this.colorsBottomEntries.length
+                                 ? this.sortedData.length - this.colorsBottomEntries.length
+                                 : this.sortedData.length - 1;
+        var leastBottomItemValue = this.sortedData[leastBottomItemIndex];
+        if (this.highlightNegativeEntries && value <= leastBottomItemValue) {
             var idx = this.sortedData.findIndex(indexFinder(value));
             var colorIndex = this.sortedData.length - 1 - idx;
             return 'style=background:' + this.colorsBottomEntries[colorIndex];
-        } else {
-            return '';
         }
+
+        return '';
     }
 
     this.highlightAscending = function(value) {
-        var topRangeCutoffIndex = Math.min(this.colorsBottomEntries.length - 1, this.sortedData.length - 1);
-        if (value >= this.sortedData[topRangeCutoffIndex]) {
+        var topRangeCutoffIndex = Math.min(this.colorsTopEntries.length - 1, this.sortedData.length - 1);
+        var leastTopItemValue = this.sortedData[topRangeCutoffIndex];
+        if (this.highlightPositiveEntries && value <= leastTopItemValue) {
             var colorIndex = this.sortedData.findIndex(indexFinder(value));
-            return 'style=background:' + this.colorsBottomEntries[colorIndex];
-        } else if (this.sortedData.length >= this.colorsTopEntries.length
-            && value <= this.sortedData[this.sortedData.length - this.colorsTopEntries.length])
-        {
+            return 'style=background:' + this.colorsTopEntries[colorIndex];
+        }
+
+        var leastBottomItemIndex = this.sortedData.length >= this.colorsBottomEntries.length
+                                 ? this.sortedData.length - this.colorsBottomEntries.length
+                                 : this.sortedData.length - 1;
+        var leastBottomItemValue = this.sortedData[leastBottomItemIndex];
+        if (this.highlightNegativeEntries && value >= leastBottomItemValue) {
             var idx = this.sortedData.findIndex(indexFinder(value));
             var colorIndex = this.sortedData.length - 1 - idx;
-            return 'style=background:' + this.colorsTopEntries[colorIndex];
-        } else {
-            return '';
+            return 'style=background:' + this.colorsBottomEntries[colorIndex];
         }
+
+        return '';
     }
 
     this.highlight = function(object) {
         if (!this.sortedData) {
-            this.sortedData = sortUniqueValues(overviewUserdata, dataKey);
+            this.sortedData = this.sortUniqueValues();
         }
 
         var value = object[dataKey];
         if (this.sortedData.length == 0 || (this.ignoreZeroes && value == 0)) {
+            return '';
+        }
+        if (this.ignoreFunction && this.ignoreFunction(object, dataKey)) {
             return '';
         }
 
@@ -89,8 +115,28 @@ function TableCellHighlighter(overviewUserdata, dataKey) {
         return this;
     }
 
+    this.setHighlightNegativeEntries = function(valueToSet) {
+        this.highlightNegativeEntries = valueToSet;
+        return this;
+    }
+
+    this.setHighlightPositiveEntries = function(valueToSet) {
+        this.highlightPositiveEntries = valueToSet;
+        return this;
+    }
+
     this.setIgnoreZeroes = function(valueToSet) {
         this.ignoreZeroes = valueToSet;
+        return this;
+    }
+
+    /**
+     * Sets the function called to check whether a value should be
+     * ignored. Two arguments are passed: element and key.
+     * Return true from the function to ignore the element.
+     */
+    this.setIgnoreFunction = function(ignoreFunction) {
+        this.ignoreFunction = ignoreFunction;
         return this;
     }
 }
